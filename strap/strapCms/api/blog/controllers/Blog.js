63f1786c54bf20827fffc4b21f0782787885689f
zinderud <mokosam@gmail.com>
2017-09-03 19:37:42 +0300

    ok

---
+++ strap/strapCms/api/blog/controllers/Blog.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const model = 'blog';
+
+/**
+ * A set of functions called "actions" for `Blog`
+ */
+
+module.exports = {
+
+  /**
+   * Get Blog entries.
+   *
+   * @return {Object|Array}
+   */
+
+  find: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.find(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Get a specific Blog.
+   *
+   * @return {Object|Array}
+   */
+
+  findOne: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.findOne(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Create a Blog entry.
+   *
+   * @return {Object}
+   */
+
+  create: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.create(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Update a Blog entry.
+   *
+   * @return {Object}
+   */
+
+  update: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.update(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Destroy a Blog entry.
+   *
+   * @return {Object}
+   */
+
+  destroy: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.destroy(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Add an entry to a specific Blog.
+   *
+   * @return {Object}
+   */
+
+  add: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.add(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  },
+
+  /**
+   * Remove a specific entry from a specific Blog.
+   *
+   * @return {Object}
+   */
+
+  remove: function * () {
+    this.model = model;
+    try {
+      let entry = yield strapi.hooks.blueprints.remove(this);
+      this.body = entry;
+    } catch (err) {
+      this.body = err;
+    }
+  }
+};
